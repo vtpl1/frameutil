77926585bd4c9fe6760af05e3cbebe19b8a8f154
Monotosh Das <monotosh.das@videonetics.com>
2024-12-03 12:12:45 +0000

    copied from grabber

--- include/frameutil.h
+++ include/frameutil.h
@@ -6,20 +6,54 @@
 #ifndef frameurtil_h
 #define frameurtil_h
 
+#include <frameurtil_export.h>
 #include <stdint.h>
+#include <string>
 
 typedef enum { UDP = 0, TCP = 1 } Protocol;
 typedef enum { H_FRAME = 0, I_FRAME = 1, P_FRAME = 2, CONNECT_HEADER = 3, AUDIO_FRAME = 16, UNKNOWN_FRAME } FrameType;
-typedef enum { VIDEO = 0, AUDIO = 1, METADATA_TYPE = 2 } PacketMedia;
-typedef enum { MJPG = 0, MPEG = 1, H264 = 2, PCMU = 3, PCMA = 4, L16 = 5, ACC = 6, UNKNOWN = 7, H265 = 8 } MediaType;
+// typedef enum { VIDEO = 0, AUDIO = 1, METADATA_TYPE = 2 } PacketMedia;
+
+typedef enum {
+  MJPEG         = 0,
+  MJPG          = 0,
+  MJPEG_TYPE    = 0,
+  MPEG          = 1,
+  MPEG_TYPE     = 1,
+  H264          = 2,
+  H264_TYPE     = 2,
+  MLAW          = 3,
+  PCMU          = 3,
+  ALAW          = 4,
+  PCMA          = 4,
+  L16           = 5,
+  ACC_CODEC     = 6,
+  ACC           = 6,
+  UNKNOWN       = 7,
+  UNKNOWN_CODEC = 6,
+  H265          = 8,
+  H265_CODEC    = 8
+} MediaType;
 
 namespace vtpl::common {
 enum NALBoundaryCondition { NO_CHANGE, NAL_ERROR, NAL_BOUNDARY_START_SPS, NAL_BOUNDARY_START_IDR, NAL_BOUNDARY_END };
-NALBoundaryCondition findNALBoundaryCondition(uint8_t last_frame_nal_type, uint8_t current_frame_nal_type,
-                                              MediaType media_type);
-uint8_t              extractNALType(uint8_t raw_byte, MediaType media_type);
+NALBoundaryCondition FRAMEURTIL_EXPORT findNALBoundaryCondition(uint8_t last_frame_nal_type,
+                                                                uint8_t current_frame_nal_type, MediaType media_type);
+uint8_t FRAMEURTIL_EXPORT              extractNALType(uint8_t raw_byte, MediaType media_type);
+
+std::string FRAMEURTIL_EXPORT getCodecString(unsigned char frameType);
+
+inline bool IS_AUDIO(int32_t x) {
+  return (x == MLAW || x == PCMU || x == ALAW || x == PCMA || x == L16 || x == ACC_CODEC || x == UNKNOWN_CODEC);
+}
+inline bool IS_VIDEO(int32_t x) { return (x == MJPG || x == H264 || x == H265); }
+
+enum H264Or5NalUnitType {
+  NAL_NONE = 0,
+};
 
 enum H264NALUnitType {
+  H264_NAL_NONE            = H264Or5NalUnitType::NAL_NONE,
   H264_NAL_SLICE           = 1,
   H264_NAL_DPA             = 2,
   H264_NAL_DPB             = 3,
@@ -39,6 +73,7 @@ enum H264NALUnitType {
  * NAL unit type codes
  */
 enum HEVCNALUnitType {
+  HEVC_NAL_NONE       = H264Or5NalUnitType::NAL_NONE,
   HEVC_NAL_TRAIL_N    = 0,
   HEVC_NAL_TRAIL_R    = 1, // P-FRAME
   HEVC_NAL_TSA_N      = 2,
@@ -65,5 +100,17 @@ enum HEVCNALUnitType {
   HEVC_NAL_SEI_PREFIX = 39, // H-FRAME
   HEVC_NAL_SEI_SUFFIX = 40
 };
+
+std::string FRAMEURTIL_EXPORT getCodecString(unsigned char frameType);
+
+class FRAMEURTIL_EXPORT NALBoundaryDetector {
+public:
+  bool isDetected(const uint8_t* data, int32_t media_type);
+
+private:
+  uint8_t last_frame_NAL_type_{NAL_NONE};
+  uint8_t last_media_type_{0};
+};
+
 } // namespace vtpl::common
 #endif // frameurtil_h
--- src/frameutil.cpp
+++ src/frameutil.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 constexpr int LAST_9_BITS  = 0x1FU;
 constexpr int LAST_10_BITS = 0x3FU;
+
 namespace vtpl::common {
 
 uint8_t extractNALType(uint8_t raw_byte, MediaType media_type) {
@@ -18,29 +19,28 @@ uint8_t extractNALType(uint8_t raw_byte, MediaType media_type) {
   return raw_byte;
 }
 
-NALBoundaryCondition findNALBoundaryCondition(uint8_t last_frame_nal_type, uint8_t current_frame_nal_type,
-                                              MediaType media_type) {
-  if (media_type == MediaType::H264) {
-    // if (last_frame_nal_type == current_frame_nal_type) {
-    //   return NALBoundaryCondition::NO_CHANGE;
-    // }
-    if ((last_frame_nal_type == H264_NAL_SLICE) && (current_frame_nal_type == H264_NAL_IDR)) {
+NALBoundaryCondition findNALBoundaryCondition(uint8_t lastFrameNALType, uint8_t currentFrameNALType,
+                                              MediaType mediaType) {
+  if (mediaType == MediaType::H264) {
+    if (((lastFrameNALType == H264_NAL_SLICE) || (lastFrameNALType == H264_NAL_NONE)) &&
+        (currentFrameNALType == H264_NAL_IDR)) {
       return NALBoundaryCondition::NAL_BOUNDARY_START_IDR;
     }
-    if ((last_frame_nal_type == H264_NAL_SLICE) && (current_frame_nal_type == H264_NAL_SPS)) {
+    if (((lastFrameNALType == H264_NAL_SLICE) || (lastFrameNALType == H264_NAL_NONE) ||
+         (lastFrameNALType == H264_NAL_AUD)) &&
+        (currentFrameNALType == H264_NAL_SPS)) {
       return NALBoundaryCondition::NAL_BOUNDARY_START_SPS;
     }
     return NALBoundaryCondition::NO_CHANGE;
   }
-  if (media_type == MediaType::H265) {
-    // if (last_frame_nal_type == current_frame_nal_type) {
-    //   return NALBoundaryCondition::NO_CHANGE;
-    // }
-    if ((last_frame_nal_type == HEVC_NAL_TRAIL_R) &&
-        ((current_frame_nal_type == HEVC_NAL_IDR_W_RADL) || (current_frame_nal_type == HEVC_NAL_IDR_N_LP))) {
+  if (mediaType == MediaType::H265) {
+    if (((lastFrameNALType == HEVC_NAL_TRAIL_R) || (lastFrameNALType == HEVC_NAL_NONE)) &&
+        (currentFrameNALType == HEVC_NAL_IDR_W_RADL)) {
       return NALBoundaryCondition::NAL_BOUNDARY_START_IDR;
     }
-    if ((last_frame_nal_type == HEVC_NAL_TRAIL_R) && (current_frame_nal_type == HEVC_NAL_VPS)) {
+    if (((lastFrameNALType == HEVC_NAL_TRAIL_R) || (lastFrameNALType == HEVC_NAL_NONE) ||
+         (lastFrameNALType == HEVC_NAL_AUD)) &&
+        (currentFrameNALType == HEVC_NAL_VPS)) {
       return NALBoundaryCondition::NAL_BOUNDARY_START_SPS;
     }
     return NALBoundaryCondition::NO_CHANGE;
@@ -48,4 +48,64 @@ NALBoundaryCondition findNALBoundaryCondition(uint8_t last_frame_nal_type, uint8
   return NALBoundaryCondition::NAL_ERROR;
 }
 
+MediaType convertVideoCodecToMediaType(int32_t media_type) {
+  switch (media_type) {
+  case 2:
+    return MediaType::H264;
+    break;
+  case 8:
+    return MediaType::H265;
+    break;
+  default:
+    break;
+  }
+  return MediaType::UNKNOWN;
+}
+
+uint8_t getNALUnitType(const uint8_t* data, int32_t media_type) {
+  return vtpl::common::extractNALType(*(data + 4), convertVideoCodecToMediaType(media_type));
+}
+
+bool findNALBoundary(uint8_t currentFrameNALType, int32_t media_type, uint8_t lastFrameNALType) {
+  return (vtpl::common::findNALBoundaryCondition(lastFrameNALType, currentFrameNALType,
+                                                 convertVideoCodecToMediaType(media_type)) ==
+          vtpl::common::NALBoundaryCondition::NAL_BOUNDARY_START_SPS);
+}
+
+std::string getCodecString(unsigned char frameType) {
+  switch (frameType) {
+  case MJPG:
+    return "MJPEG";
+  case MPEG:
+    return "MPEG";
+  case H264:
+    return "H264";
+  case PCMU:
+    return "PCMU";
+  case PCMA:
+    return "PCMA";
+  case L16:
+    return "L16";
+  case ACC:
+    return "ACC";
+  case H265:
+    return "H265";
+  default:
+    return "UNKN";
+  }
+}
+bool NALBoundaryDetector::isDetected(const uint8_t* data, int32_t media_type) {
+  if (!IS_VIDEO(media_type)) {
+    return false;
+  }
+
+  const uint8_t currentNALUnitType = getNALUnitType(data, media_type);
+  if (last_media_type_ != media_type) {
+    last_frame_NAL_type_ = NAL_NONE;
+  }
+  const bool is_nal_boundary_detected = findNALBoundary(currentNALUnitType, media_type, last_frame_NAL_type_);
+  last_frame_NAL_type_                = currentNALUnitType;
+  last_media_type_                    = media_type;
+  return is_nal_boundary_detected;
+}
 } // namespace vtpl::common
