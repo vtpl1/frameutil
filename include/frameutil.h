9056985c211f878ebf12ff6498b7bbe7640bca71
Monotosh Das <monotosh.das@videonetics.com>
2025-07-01 07:33:04 +0000

    isKeyFrame, isDataFrame added

--- include/frameutil.h
+++ include/frameutil.h
@@ -42,52 +42,109 @@ enum H264Or5NalUnitType {
 };
 
 enum H264NALUnitType {
-  H264_NAL_NONE            = H264Or5NalUnitType::NAL_NONE,
-  H264_NAL_SLICE           = 1,
-  H264_NAL_DPA             = 2,
-  H264_NAL_DPB             = 3,
-  H264_NAL_DPC             = 4,
-  H264_NAL_IDR             = 5, // I-FRAME
-  H264_NAL_SEI             = 6,
-  H264_NAL_SPS             = 7, // CONNECT-HEADER
-  H264_NAL_PPS             = 8, // CONNECT-HEADER
-  H264_NAL_AUD             = 9,
-  H264_NAL_END_SEQUENCE    = 10,
-  H264_NAL_END_STREAM      = 11,
-  H264_NAL_FILLER_DATA     = 12,
-  H264_NAL_SPS_EXT         = 13,
-  H264_NAL_AUXILIARY_SLICE = 19
+  H264_NAL_NONE              = H264Or5NalUnitType::NAL_NONE,
+  H264_NAL_SLICE             = 1,
+  H264_NAL_DPA               = 2,
+  H264_NAL_DPB               = 3,
+  H264_NAL_DPC               = 4,
+  H264_NAL_IDR               = 5, // I-FRAME
+  H264_NAL_SEI               = 6,
+  H264_NAL_SPS               = 7, // CONNECT-HEADER
+  H264_NAL_PPS               = 8, // CONNECT-HEADER
+  H264_NAL_AUD               = 9,
+  H264_NAL_END_SEQUENCE      = 10,
+  H264_NAL_END_STREAM        = 11,
+  H264_NAL_FILLER_DATA       = 12,
+  H264_NAL_SPS_EXT           = 13,
+  H264_NAL_PREFIX            = 14,
+  H264_NAL_SUB_SPS           = 15,
+  H264_NAL_DPS               = 16,
+  H264_NAL_RESERVED17        = 17,
+  H264_NAL_RESERVED18        = 18,
+  H264_NAL_AUXILIARY_SLICE   = 19,
+  H264_NAL_EXTEN_SLICE       = 20,
+  H264_NAL_DEPTH_EXTEN_SLICE = 21,
+  H264_NAL_RESERVED22        = 22,
+  H264_NAL_RESERVED23        = 23,
+  H264_NAL_UNSPECIFIED24     = 24,
+  H264_NAL_UNSPECIFIED25     = 25,
+  H264_NAL_UNSPECIFIED26     = 26,
+  H264_NAL_UNSPECIFIED27     = 27,
+  H264_NAL_UNSPECIFIED28     = 28,
+  H264_NAL_UNSPECIFIED29     = 29,
+  H264_NAL_UNSPECIFIED30     = 30,
+  H264_NAL_UNSPECIFIED31     = 31
 };
 /**
  * NAL unit type codes
  */
 enum HEVCNALUnitType {
-  HEVC_NAL_NONE       = H264Or5NalUnitType::NAL_NONE,
-  HEVC_NAL_TRAIL_N    = 0,
-  HEVC_NAL_TRAIL_R    = 1, // P-FRAME
-  HEVC_NAL_TSA_N      = 2,
-  HEVC_NAL_TSA_R      = 3,
-  HEVC_NAL_STSA_N     = 4,
-  HEVC_NAL_STSA_R     = 5,
-  HEVC_NAL_RADL_N     = 6,
-  HEVC_NAL_RADL_R     = 7,
-  HEVC_NAL_RASL_N     = 8,
-  HEVC_NAL_RASL_R     = 9,
-  HEVC_NAL_BLA_W_LP   = 16,
-  HEVC_NAL_BLA_W_RADL = 17,
-  HEVC_NAL_BLA_N_LP   = 18,
-  HEVC_NAL_IDR_W_RADL = 19, // I-FRAME
-  HEVC_NAL_IDR_N_LP   = 20,
-  HEVC_NAL_CRA_NUT    = 21,
-  HEVC_NAL_VPS        = 32, // CONNECT-HEADER
-  HEVC_NAL_SPS        = 33, // CONNECT-HEADER
-  HEVC_NAL_PPS        = 34, // CONNECT-HEADER
-  HEVC_NAL_AUD        = 35,
-  HEVC_NAL_EOS_NUT    = 36,
-  HEVC_NAL_EOB_NUT    = 37,
-  HEVC_NAL_FD_NUT     = 38,
-  HEVC_NAL_SEI_PREFIX = 39, // H-FRAME
-  HEVC_NAL_SEI_SUFFIX = 40
+  HEVC_NAL_NONE           = H264Or5NalUnitType::NAL_NONE,
+  HEVC_NAL_TRAIL_N        = 0,
+  HEVC_NAL_TRAIL_R        = 1, // P-FRAME
+  HEVC_NAL_TSA_N          = 2,
+  HEVC_NAL_TSA_R          = 3,
+  HEVC_NAL_STSA_N         = 4,
+  HEVC_NAL_STSA_R         = 5,
+  HEVC_NAL_RADL_N         = 6,
+  HEVC_NAL_RADL_R         = 7,
+  HEVC_NAL_RASL_N         = 8,
+  HEVC_NAL_RASL_R         = 9,
+  HEVC_NAL_VCL_N10        = 10,
+  HEVC_NAL_VCL_R11        = 11,
+  HEVC_NAL_VCL_N12        = 12,
+  HEVC_NAL_VCL_R13        = 13,
+  HEVC_NAL_VCL_N14        = 14,
+  HEVC_NAL_VCL_R15        = 15,
+  HEVC_NAL_BLA_W_LP       = 16,
+  HEVC_NAL_BLA_W_RADL     = 17,
+  HEVC_NAL_BLA_N_LP       = 18,
+  HEVC_NAL_IDR_W_RADL     = 19, // I-FRAME
+  HEVC_NAL_IDR_N_LP       = 20,
+  HEVC_NAL_CRA_NUT        = 21,
+  HEVC_NAL_RSV_IRAP_VCL22 = 22,
+  HEVC_NAL_RSV_IRAP_VCL23 = 23,
+  HEVC_NAL_RSV_VCL24      = 24,
+  HEVC_NAL_RSV_VCL25      = 25,
+  HEVC_NAL_RSV_VCL26      = 26,
+  HEVC_NAL_RSV_VCL27      = 27,
+  HEVC_NAL_RSV_VCL28      = 28,
+  HEVC_NAL_RSV_VCL29      = 29,
+  HEVC_NAL_RSV_VCL30      = 30,
+  HEVC_NAL_RSV_VCL31      = 31,
+  HEVC_NAL_VPS            = 32, // CONNECT-HEADER
+  HEVC_NAL_SPS            = 33, // CONNECT-HEADER
+  HEVC_NAL_PPS            = 34, // CONNECT-HEADER
+  HEVC_NAL_AUD            = 35,
+  HEVC_NAL_EOS_NUT        = 36,
+  HEVC_NAL_EOB_NUT        = 37,
+  HEVC_NAL_FD_NUT         = 38,
+  HEVC_NAL_SEI_PREFIX     = 39, // H-FRAME
+  HEVC_NAL_SEI_SUFFIX     = 40,
+  HEVC_NAL_RSV_NVCL41     = 41,
+  HEVC_NAL_RSV_NVCL42     = 42,
+  HEVC_NAL_RSV_NVCL43     = 43,
+  HEVC_NAL_RSV_NVCL44     = 44,
+  HEVC_NAL_RSV_NVCL45     = 45,
+  HEVC_NAL_RSV_NVCL46     = 46,
+  HEVC_NAL_RSV_NVCL47     = 47,
+  HEVC_NAL_UNSPEC48       = 48,
+  HEVC_NAL_UNSPEC49       = 49,
+  HEVC_NAL_UNSPEC50       = 50,
+  HEVC_NAL_UNSPEC51       = 51,
+  HEVC_NAL_UNSPEC52       = 52,
+  HEVC_NAL_UNSPEC53       = 53,
+  HEVC_NAL_UNSPEC54       = 54,
+  HEVC_NAL_UNSPEC55       = 55,
+  HEVC_NAL_UNSPEC56       = 56,
+  HEVC_NAL_UNSPEC57       = 57,
+  HEVC_NAL_UNSPEC58       = 58,
+  HEVC_NAL_UNSPEC59       = 59,
+  HEVC_NAL_UNSPEC60       = 60,
+  HEVC_NAL_UNSPEC61       = 61,
+  HEVC_NAL_UNSPEC62       = 62,
+  HEVC_NAL_UNSPEC63       = 63
+
 };
 
 // NALBoundaryCondition FRAMEUTIL_EXPORT findNALBoundaryCondition(uint8_t last_frame_nal_type,
@@ -97,6 +154,8 @@ inline bool IS_AUDIO(int32_t x) {
 }
 inline bool IS_VIDEO(int32_t x) { return (x == MJPG || x == H264 || x == H265); }
 
+bool FRAMEUTIL_EXPORT        isDataNALType(uint8_t raw_nal_byte, MediaType media_type);
+bool FRAMEUTIL_EXPORT        isKeyFrame(uint8_t raw_nal_byte, MediaType media_type);
 uint8_t FRAMEUTIL_EXPORT     extractNALType(uint8_t raw_nal_byte, MediaType media_type);
 std::string FRAMEUTIL_EXPORT extractNALTypeString(uint8_t raw_nal_byte, MediaType media_type);
 std::string FRAMEUTIL_EXPORT getCodecString(MediaType media_type);
--- src/frameutil.cpp
+++ src/frameutil.cpp
@@ -21,6 +21,60 @@ uint8_t extractNALType(uint8_t raw_nal_byte, MediaType media_type) {
   return raw_nal_byte;
 }
 
+bool isDataNALType(uint8_t raw_nal_byte, MediaType media_type) {
+  if (vtpl::common::MediaType::H264 == media_type) {
+    uint8_t nal_type = vtpl::common::extractNALType(raw_nal_byte, media_type);
+    if (nal_type == H264_NAL_IDR) {
+      return true;
+    }
+    if ((nal_type == H264_NAL_SPS) ||
+        (nal_type == H264_NAL_PPS) /*|| (nal_unti_type == H264_NAL_SEI)*/) { // FIXME(Monotosh)
+      return false;
+    }
+    if (nal_type < H264_NAL_IDR) {
+      return true;
+    }
+    return false;
+  }
+
+  if (vtpl::common::MediaType::H265 == media_type) {
+    uint8_t nal_type = vtpl::common::extractNALType(raw_nal_byte, media_type);
+    if ((nal_type == HEVC_NAL_IDR_W_RADL) || (nal_type == HEVC_NAL_IDR_N_LP)) {
+      return true;
+    }
+    if ((nal_type == HEVC_NAL_SPS) || (nal_type == HEVC_NAL_PPS) || (nal_type == HEVC_NAL_VPS)) {
+      return false;
+    }
+    if (nal_type < 31) {
+      return true;
+    }
+    return false;
+  }
+
+  if (vtpl::common::MediaType::MJPG == media_type) {
+    return true;
+  }
+  return false;
+}
+
+bool isKeyFrame(uint8_t raw_nal_byte, MediaType media_type) {
+  const uint8_t nal_type = extractNALType(raw_nal_byte, media_type);
+
+  bool is_idr_detected = false;
+
+  if (media_type == MediaType::H264) {
+    if (nal_type == H264_NAL_IDR) {
+      is_idr_detected = true;
+    }
+  }
+  if (media_type == MediaType::H265) {
+    if ((nal_type == HEVC_NAL_IDR_W_RADL) || (nal_type == HEVC_NAL_IDR_N_LP)) {
+      is_idr_detected = true;
+    }
+  }
+  return is_idr_detected;
+}
+
 std::string extractNALTypeString(uint8_t raw_nal_byte, MediaType media_type) {
   if (UNKNOWN == media_type) {
     return "UNKNOWN";
@@ -77,8 +131,59 @@ std::string extractNALTypeString(uint8_t raw_nal_byte, MediaType media_type) {
     case H264_NAL_SPS_EXT:
       return "H264_SPS_EXT";
       break;
+    case H264_NAL_PREFIX:
+      return "H264_PREFIX";
+      break;
+    case H264_NAL_SUB_SPS:
+      return "H264_SUB_SPS";
+      break;
+    case H264_NAL_DPS:
+      return "H264_DPS";
+      break;
+    case H264_NAL_RESERVED17:
+      return "H264_RESERVED17";
+      break;
+    case H264_NAL_RESERVED18:
+      return "H264_RESERVED18";
+      break;
     case H264_NAL_AUXILIARY_SLICE:
       return "H264_AUXILIARY_SLICE";
+      break;
+    case H264_NAL_EXTEN_SLICE:
+      return "H264_EXTEN_SLICE";
+      break;
+    case H264_NAL_DEPTH_EXTEN_SLICE:
+      return "H264_DEPTH_EXTEN_SLICE";
+      break;
+    case H264_NAL_RESERVED22:
+      return "H264_RESERVED22";
+      break;
+    case H264_NAL_RESERVED23:
+      return "H264_RESERVED23";
+      break;
+    case H264_NAL_UNSPECIFIED24:
+      return "H264_UNSPECIFIED24";
+      break;
+    case H264_NAL_UNSPECIFIED25:
+      return "H264_UNSPECIFIED25";
+      break;
+    case H264_NAL_UNSPECIFIED26:
+      return "H264_UNSPECIFIED26";
+      break;
+    case H264_NAL_UNSPECIFIED27:
+      return "H264_UNSPECIFIED27";
+      break;
+    case H264_NAL_UNSPECIFIED28:
+      return "H264_UNSPECIFIED28";
+      break;
+    case H264_NAL_UNSPECIFIED29:
+      return "H264_UNSPECIFIED29";
+      break;
+    case H264_NAL_UNSPECIFIED30:
+      return "H264_UNSPECIFIED30";
+      break;
+    case H264_NAL_UNSPECIFIED31:
+      return "H264_UNSPECIFIED31";
       break;
     default:
       return "H264_UNKNOWN";
@@ -87,8 +192,8 @@ std::string extractNALTypeString(uint8_t raw_nal_byte, MediaType media_type) {
   if (H265 == media_type) {
     auto nalu1 = static_cast<HEVCNALUnitType>(nalu);
     switch (nalu1) {
-    case HEVC_NAL_NONE:
-      return "HEVC_NONE";
+    case HEVC_NAL_TRAIL_N:
+      return "HEVC_TRAIL_N";
       break;
     case HEVC_NAL_TRAIL_R:
       return "HEVC_TRAIL_R";
@@ -117,6 +222,24 @@ std::string extractNALTypeString(uint8_t raw_nal_byte, MediaType media_type) {
     case HEVC_NAL_RASL_R:
       return "HEVC_RASL_R";
       break;
+    case HEVC_NAL_VCL_N10:
+      return "HEVC_VCL_N10";
+      break;
+    case HEVC_NAL_VCL_R11:
+      return "HEVC_VCL_R11";
+      break;
+    case HEVC_NAL_VCL_N12:
+      return "HEVC_VCL_N12";
+      break;
+    case HEVC_NAL_VCL_R13:
+      return "HEVC_VCL_R13";
+      break;
+    case HEVC_NAL_VCL_N14:
+      return "HEVC_VCL_N14";
+      break;
+    case HEVC_NAL_VCL_R15:
+      return "HEVC_VCL_R15";
+      break;
     case HEVC_NAL_BLA_W_LP:
       return "HEVC_BLA_W_LP";
       break;
@@ -134,6 +257,36 @@ std::string extractNALTypeString(uint8_t raw_nal_byte, MediaType media_type) {
       break;
     case HEVC_NAL_CRA_NUT:
       return "HEVC_CRA_NUT";
+      break;
+    case HEVC_NAL_RSV_IRAP_VCL22:
+      return "HEVC_RSV_IRAP_VCL22";
+      break;
+    case HEVC_NAL_RSV_IRAP_VCL23:
+      return "HEVC_RSV_IRAP_VCL23";
+      break;
+    case HEVC_NAL_RSV_VCL24:
+      return "HEVC_RSV_VCL24";
+      break;
+    case HEVC_NAL_RSV_VCL25:
+      return "HEVC_RSV_VCL25";
+      break;
+    case HEVC_NAL_RSV_VCL26:
+      return "HEVC_RSV_VCL26";
+      break;
+    case HEVC_NAL_RSV_VCL27:
+      return "HEVC_RSV_VCL27";
+      break;
+    case HEVC_NAL_RSV_VCL28:
+      return "HEVC_RSV_VCL28";
+      break;
+    case HEVC_NAL_RSV_VCL29:
+      return "HEVC_RSV_VCL29";
+      break;
+    case HEVC_NAL_RSV_VCL30:
+      return "HEVC_RSV_VCL30";
+      break;
+    case HEVC_NAL_RSV_VCL31:
+      return "HEVC_RSV_VCL31";
       break;
     case HEVC_NAL_VPS:
       return "HEVC_VPS";
@@ -161,6 +314,75 @@ std::string extractNALTypeString(uint8_t raw_nal_byte, MediaType media_type) {
       break;
     case HEVC_NAL_SEI_SUFFIX:
       return "HEVC_SEI_SUFFIX";
+      break;
+    case HEVC_NAL_RSV_NVCL41:
+      return "HEVC_RSV_NVCL41";
+      break;
+    case HEVC_NAL_RSV_NVCL42:
+      return "HEVC_RSV_NVCL42";
+      break;
+    case HEVC_NAL_RSV_NVCL43:
+      return "HEVC_RSV_NVCL43";
+      break;
+    case HEVC_NAL_RSV_NVCL44:
+      return "HEVC_RSV_NVCL44";
+      break;
+    case HEVC_NAL_RSV_NVCL45:
+      return "HEVC_RSV_NVCL45";
+      break;
+    case HEVC_NAL_RSV_NVCL46:
+      return "HEVC_RSV_NVCL46";
+      break;
+    case HEVC_NAL_RSV_NVCL47:
+      return "HEVC_RSV_NVCL47";
+      break;
+    case HEVC_NAL_UNSPEC48:
+      return "HEVC_UNSPEC48";
+      break;
+    case HEVC_NAL_UNSPEC49:
+      return "HEVC_UNSPEC49";
+      break;
+    case HEVC_NAL_UNSPEC50:
+      return "HEVC_UNSPEC50";
+      break;
+    case HEVC_NAL_UNSPEC51:
+      return "HEVC_UNSPEC51";
+      break;
+    case HEVC_NAL_UNSPEC52:
+      return "HEVC_UNSPEC52";
+      break;
+    case HEVC_NAL_UNSPEC53:
+      return "HEVC_UNSPEC53";
+      break;
+    case HEVC_NAL_UNSPEC54:
+      return "HEVC_UNSPEC54";
+      break;
+    case HEVC_NAL_UNSPEC55:
+      return "HEVC_UNSPEC55";
+      break;
+    case HEVC_NAL_UNSPEC56:
+      return "HEVC_UNSPEC56";
+      break;
+    case HEVC_NAL_UNSPEC57:
+      return "HEVC_UNSPEC57";
+      break;
+    case HEVC_NAL_UNSPEC58:
+      return "HEVC_UNSPEC58";
+      break;
+    case HEVC_NAL_UNSPEC59:
+      return "HEVC_UNSPEC59";
+      break;
+    case HEVC_NAL_UNSPEC60:
+      return "HEVC_UNSPEC60";
+      break;
+    case HEVC_NAL_UNSPEC61:
+      return "HEVC_UNSPEC61";
+      break;
+    case HEVC_NAL_UNSPEC62:
+      return "HEVC_UNSPEC62";
+      break;
+    case HEVC_NAL_UNSPEC63:
+      return "HEVC_UNSPEC63";
       break;
     default:
       return "HEVC_UNKNOWN";
